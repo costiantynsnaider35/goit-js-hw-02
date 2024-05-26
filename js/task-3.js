a18a5d08248c296bdd76072a5aaee4c66bb03a2c
Kostiantyn Shnaider <constantin161089@gmail.com>
2024-05-26 13:34:06 +0300

    дз2

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function checkForSpam(message) {
+  const bannedWord1 = "spam";
+  const bannedWord2 = "sale";
+  const messageLowerCase = message.toLowerCase();
+  const result =
+    messageLowerCase.includes(bannedWord1) ||
+    messageLowerCase.includes(bannedWord2);
+  return result;
+}
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
